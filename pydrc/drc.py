bb4f4b89a3dcf19e4f26e346d4ad34191e7e0a96
Scott Warchal <scott.warchal@gmail.com>
2022-05-12 20:38:55 +0100

    add y-maximal scaling option, and plot x scale option

--- pydrc/drc.py
+++ pydrc/drc.py
@@ -2,7 +2,7 @@
 Actual dose response fitting classes.
 """
 
-from typing import Optional, NamedTuple
+from typing import Optional, NamedTuple, Tuple
 
 import numpy as np
 import pandas as pd
@@ -24,10 +24,12 @@ class BaseDRC:
         iter: int = 500,
         init: Optional[ArrayLike] = None,
         bounds: Optional[ArrayLike] = None,
+        rescale: bool = False,
     ):
         self.x = None
         self.y = None
         self.c = None
+        self.rescale = rescale
         self.method = "trf"
         self.iter = iter
         self.init = init
@@ -41,7 +43,25 @@ class BaseDRC:
     def _fit(self, x, y, c=None):
         raise NotImplementedError
 
+    def do_rescaling(self, y: ArrayLike, c: Optional[ArrayLike] = None) -> ArrayLike:
+        """rescale each response between 0 and 100"""
+        y = np.asarray(y)
+
+        def _rescale_group(df: pd.DataFrame) -> pd.DataFrame:
+            ymin = df.y.min()
+            ymax = df.y.max()
+            df.y = (df.y - ymin) / (ymax - ymin) * 100
+            return df
+
+        if c is not None:
+            df = pd.DataFrame({"y": y, "c": c})
+            df = df.groupby("c").apply(_rescale_group)
+            return df.y.values
+        return (y - y.min()) / (y.max() - y.min()) * 100
+
     def fit(self, x: ArrayLike, y: ArrayLike, c: Optional[ArrayLike] = None):
+        if self.rescale:
+            y = self.do_rescaling(y, c)
         self.x, self.y, self.c = x, y, c
         param_store = {}
         if c is not None:
@@ -52,7 +72,7 @@ class BaseDRC:
         else:
             self.param_store = self._fit(x, y)
 
-    def plot(self):
+    def plot(self, xscale="log"):
         assert self.x is not None and self.y is not None
         assert self.param_store is not None
         xmin, xmax = min(self.x), max(self.x)
@@ -61,7 +81,7 @@ class BaseDRC:
             df = pd.DataFrame({"x": self.x, "y": self.y, "c": self.c})
             for name, group in df.groupby("c"):
                 plt.scatter(group.x, group.y, label=name)
-            plt.xscale("log")
+            plt.xscale(xscale)
             for i in self.param_store.keys():
                 y_interp = self.model(x_interp, *self.param_store[i])
                 plt.plot(x_interp, y_interp, label=i)
@@ -69,7 +89,7 @@ class BaseDRC:
             plt.show()
         else:
             plt.scatter(self.x, self.y)
-            plt.xscale("log")
+            plt.xscale(xscale)
             y_interp = self.model(x_interp, *self.param_store)
             plt.plot(x_interp, y_interp)
             plt.show()
